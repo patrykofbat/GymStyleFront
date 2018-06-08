0ee5df070394e5ab76a0bff2a25fee3677760de0
Patryk Papiór <patrykpapior1997@gmail.com>
2018-06-08 16:52:13 +0200

    refactor code became more DRY

--- src/components/dashboard/training/Training.jsx
+++ src/components/dashboard/training/Training.jsx
@@ -51,6 +51,25 @@ class Training extends Component {
     });
   };
 
+  changeTable = (source, destination, result) =>{
+    let removed = source.splice(result.source.index, 1);
+    let newItem = {
+      id: result.draggableId,
+      content: removed[0].content,
+      index: result.destination.index,
+      key: removed[0].key,
+      link: removed[0].link,
+      img: removed[0].img,
+      description: removed[0].description
+    };
+    source = this.reIndexDeleted(
+      source,
+      result.source.index
+    );
+    destination = this.reIndexAdded(destination, newItem);
+
+  }
+
   changeOption = currentId => {
     if(this.props.requestedIds.includes(currentId)){
       this.setState({
@@ -69,83 +88,24 @@ class Training extends Component {
       return;
     }
 
-    if (
-      result.destination.droppableId === "training" &&
-      result.source.droppableId !== "training"
-    ) {
-      this.setState((prevState, props) => {
-        let removed = prevState.items.splice(result.source.index, 1);
-        let newItem = {
-          id: result.draggableId,
-          content: removed[0].content,
-          index: result.destination.index,
-          key: removed[0].key,
-          link: removed[0].link,
-          img: removed[0].img,
-          description: removed[0].description
-        };
-        prevState.items = this.reIndexDeleted(
-          prevState.items,
-          result.source.index
-        );
-        prevState.trainings = this.reIndexAdded(prevState.trainings, newItem);
-        return {
-          ...prevState
-        };
-      });
-    } else if (
-      result.destination.droppableId === "exercise" &&
-      result.source.droppableId !== "exercise"
-    ) {
-      this.setState((prevState, props) => {
-        let removed = prevState.trainings.splice(result.source.index, 1);
-        let newItem = {
-          id: result.draggableId,
-          content: removed[0].content,
-          index: result.destination.index,
-          key: removed[0].key,
-          link: removed[0].link,
-          img: removed[0].img,
-          description: removed[0].description
-        };
-        prevState.trainings = this.reIndexDeleted(
-          prevState.trainings,
-          result.source.index
-        );
-        prevState.items = this.reIndexAdded(prevState.items, newItem);
-        return {
-          ...prevState
-        };
-      });
-    } else if (
-      result.destination.droppableId === "exercise" &&
-      result.source.droppableId === "exercise"
-    ) {
-      this.setState((prevState, props) => {
-        let picked = prevState.items.splice(result.source.index, 1);
-        picked[0].index = result.destination.index;
-        prevState.items = this.reIndexDeleted(
-          prevState.items,
-          result.source.index
-        );
-        prevState.items = this.reIndexAdded(prevState.items, picked[0]);
-        return { ...prevState };
-      });
-    } else if (
-      result.destination.droppableId === "training" &&
-      result.source.droppableId === "training"
-    ) {
-      this.setState((prevState, props) => {
-        let picked = prevState.trainings.splice(result.source.index, 1);
-        picked[0].index = result.destination.index;
-        prevState.trainings = this.reIndexDeleted(
-          prevState.trainings,
-          result.source.index
-        );
-        prevState.tranings = this.reIndexAdded(prevState.trainings, picked[0]);
-        return { ...prevState };
+    if(result.destination.droppableId !== result.source.droppableId){
+      this.setState((prevState) => {
+        if(result.destination.droppableId === "training")
+          this.changeTable(prevState.items, prevState.trainings, result);
+        else
+          this.changeTable(prevState.trainings, prevState.items, result);
+        return {...prevState};
       });
     }
+    else{
+      this.setState((prevState) =>{
+        if(result.destination.droppableId === "exercise")
+          this.changeTable(prevState.items, prevState.items, result);
+        else
+          this.changeTable(prevState.trainings, prevState.trainings, result);
+      });
+    }
+  
   };
 
   render() {
