e9e889d57a023c1071d887075ae4dae74b4dce12
Patryk <patrykpapior1997@gmail.com>
2018-07-23 22:59:03 +0200

    apply some css to PopUp

--- src/components/common/PopUp.jsx
+++ src/components/common/PopUp.jsx
@@ -1,31 +1,11 @@
 import React, { Component } from "react";
 
 class PopUp extends Component {
-  style = {
-    position: "fixed",
-    display: "flex",
-    flexDirection: "column",
-    backgroundColor: "#F7F7F7",
-    boxSizing: "border-box",
-    width: "70%",
-    height: "69%",
-    marginLeft: "1.5%",
-    marginTop: "1.5%",
-    borderRadius: "0.4rem",
-    border: "1px solid #d4d4d5"
-  };
 
   render() {
     return (
-      <div style={this.style}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row-reverse",
-            justifyContent: "space-between",
-            margin: "0.5rem"
-          }}
-        >
+      <div className="PopUpBg">
+        <div>
           <button onClick={this.props.esc}>x</button>
           <p style={{ fontSize: "1.5em" }}>{this.props.title}</p>
         </div>
